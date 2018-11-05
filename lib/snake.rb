1008823d434e242f25b57f26c758aa1c700ae3d2
Lean <lndr.figueredo@gmail.com>
2018-11-05 14:44:27 -0200

    Adjust pos and draw_rect in snake class

--- lib/snake.rb
+++ lib/snake.rb
@@ -1,26 +1,54 @@
 # snake player class
 class Snake
   attr_accessor :x, :y
+  attr_reader :pos
+
+  MIN_TAIL = 3.freeze
+
   def initialize
     @x     = 10
     @y     = 10
+
     @vel_x = 0
     @vel_y = 0
+
+    @tail = MIN_TAIL
+
+    @pos = []
   end
 
   def update
     @x += @vel_x
     @y += @vel_y
+
+    @x = 0 if @x > SnakeGame::WIDTH_IN_TILE
+    @x = SnakeGame::WIDTH_IN_TILE if @x < 0
+    @y = 0 if @y > SnakeGame::WIDTH_IN_TILE
+    @y = SnakeGame::WIDTH_IN_TILE if @y < 0
+
+    @pos << [@x, @y]
+
+    @pos.shift until @pos.size <= @tail
   end
 
   def draw
-    Gosu.draw_rect(
-      x * SnakeGame::TILE,
-      y * SnakeGame::TILE,
-      SnakeGame::TILE - 1,
-      SnakeGame::TILE - 1,
-      Gosu::Color::GREEN
-    )
+    @pos.each do |x, y|
+      Gosu.draw_rect(
+        x * SnakeGame::TILE,
+        y * SnakeGame::TILE,
+        SnakeGame::TILE - 1,
+        SnakeGame::TILE - 1,
+        Gosu::Color::GREEN
+      )
+    end
+  end
+
+  def reset
+    @tail = MIN_TAIL
+  end
+
+  def increase
+    @tail += 1
   end
 
   def up
